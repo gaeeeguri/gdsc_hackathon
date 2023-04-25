d749a590b151f4cb2755fea51bad1fab233e4a6d
gaeeeguri <sjpark513@gm.gist.ac.kr>
2023-04-26 00:13:00 +0900

    init project

---
+++ src/main/java/com/sgp/gdsc_hackathon/GdscHackathonApplication.java
@@ -0,0 +1,13 @@
+package com.sgp.gdsc_hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdscHackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdscHackathonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sgp/gdsc_hackathon/GdscHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgp.gdsc_hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdscHackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
