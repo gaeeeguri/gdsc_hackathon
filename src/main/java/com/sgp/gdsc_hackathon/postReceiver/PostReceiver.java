e95a98932d86ab9c4a4be06d160e5418e5b4e1f6
gaeeeguri <sjpark513@gm.gist.ac.kr>
2023-04-29 21:33:42 +0900

    feat(postReceiver): create postReceiver

--- src/main/java/com/sgp/gdsc_hackathon/postReceiver/PostReceiver.java
+++ src/main/java/com/sgp/gdsc_hackathon/postReceiver/PostReceiver.java
@@ -1,2 +1,26 @@
-package com.sgp.gdsc_hackathon.postReciever;public class PostReciver {
+package com.sgp.gdsc_hackathon.postReceiver;
+
+import com.sgp.gdsc_hackathon.post.Post;
+import com.sgp.gdsc_hackathon.user.User;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class PostReceiver {
+    @Id
+    @Column(name = "post_receiver_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
 }
