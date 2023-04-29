9f0c2c5f370f1185c2c48593825f0cd96392e0d2
gaeeeguri <sjpark513@gm.gist.ac.kr>
2023-04-29 20:57:07 +0900

    docs(user,-post): add swagger

--- src/main/java/com/sgp/gdsc_hackathon/post/PostController.java
+++ src/main/java/com/sgp/gdsc_hackathon/post/PostController.java
@@ -1,5 +1,6 @@
 package com.sgp.gdsc_hackathon.post;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,11 +13,13 @@ public class PostController {
     }
 
     @GetMapping("/posts/{user_id}")
+    @Operation(summary = "Get posts of a user", description = "Returns posts of given user id")
     public Iterable<Post> getPosts(@PathVariable("user_id") Long userId) {
         return postService.findUserPosts(userId);
     }
 
     @PostMapping("/posts")
+    @Operation(summary = "create post", description = "Create a new post and return id")
     public Long createPost(@RequestBody Post post) {
         return postService.upload(post);
     }
--- src/main/java/com/sgp/gdsc_hackathon/user/UserController.java
+++ src/main/java/com/sgp/gdsc_hackathon/user/UserController.java
@@ -1,5 +1,6 @@
 package com.sgp.gdsc_hackathon.user;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,11 +18,13 @@ public class UserController {
     }
 
     @GetMapping("/users")
+    @Operation(summary = "get users", description = "Returns all users")
     public Iterable<User> getUsers() {
         return userService.findUsers();
     }
 
     @PostMapping("/users")
+    @Operation(summary = "create user", description = "Create a new user and returns id")
     public Long createUser(@RequestBody User user) {
         return userService.join(user);
     }
